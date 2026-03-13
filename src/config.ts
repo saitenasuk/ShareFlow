e8f643509ebdc2d6883e272469e5b4741b27df4b
saitenasuk <saitenasuk@gmail.com>
2026-03-13 22:50:19 +0800

    chore: update config AUTH_PASSWORD

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const getConfig = (env?: Record<string, string>) => ({
   MAX_TEXT_LENGTH: parseInt(env?.MAX_TEXT_LENGTH || process.env.MAX_TEXT_LENGTH || '100000', 10),
   PORT: parseInt(env?.PORT || process.env.PORT || '3000', 10),
   DATA_DIR: env?.DATA_DIR || process.env.DATA_DIR || './data',
-  AUTH_PASSWORD: env?.AUTH_PASSWORD || process.env.AUTH_PASSWORD || '123',
+  AUTH_PASSWORD: env?.AUTH_PASSWORD || process.env.AUTH_PASSWORD || '',
 })
 
 export type AppConfig = ReturnType<typeof getConfig>
